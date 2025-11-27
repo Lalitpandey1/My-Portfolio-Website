841636c14b4661b30f3608b4a825b49dedcf9661
Lalitpandey1 <154809463+Lalitpandey1@users.noreply.github.com>
2025-11-27 18:03:53 +0530

    EmailJS integrated

---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,225 @@
+import { Instagram, Linkedin, Mail, MapPin, Phone, Send } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useToast } from "@/hooks/use-toast";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+export const ContactSection = () => {
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { toast } = useToast();
+  const templateParams = {
+    from_name: name,
+    from_email: email,
+    message: message,
+    to_mail: "lalitpandey.lp39@gmail.com",
+    to_name: "Lalit Pandey",
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const publicKey = "0tHRQcpI-GWvQXmfJ";
+    const serviceId = "service_wob1184";
+    const templateId = "template_2fivijg";
+    emailjs
+      .send(serviceId, templateId, templateParams, {
+        //instead of sendForms
+        publicKey: publicKey,
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+
+    setTimeout(() => {
+      toast({
+        title: "Message sent!",
+        description: "Thank you for your message. I'll get back to you soon.",
+      });
+      setIsSubmitting(false);
+    }, 1500);
+  };
+
+  return (
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=lalitpandey.lp39@gmail.com">
+            {" "}
+            Get In <span className="text-primary"> Touch</span>
+          </a>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Have a project in mind or want to collaborate? Feel free to reach out.
+          I am always open to discussing new opportunities.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h3 className="text-2xl font-semibold mb-6">
+              {" "}
+              Contact Information
+            </h3>
+
+            <div className="space-y-6 justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div className="">
+                  <h4 className="font-medium"> Email</h4>
+                  <a
+                    href="mailto:hello@gmail.com"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    lalitpandey.lp39@gmail.com
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Phone</h4>
+                  <a
+                    href="tel:+11234567890"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    +91 8861716528
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <MapPin className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Location</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                    Bangalore, India
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="pt-8">
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
+              <div className="flex space-x-4 justify-center">
+                <a
+                  href="https://www.linkedin.com/in/lalit-pandey-5a0318225/"
+                  target="_blank"
+                >
+                  <Linkedin />
+                </a>
+                <a
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=lalitpandey.lp39@gmail.com"
+                  target="_blank"
+                >
+                  <Mail />
+                </a>
+                <a
+                  href="https://www.instagram.com/new_spark_11/"
+                  target="_blank"
+                >
+                  <Instagram />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="bg-card p-8 rounded-lg shadow-xs"
+            onSubmit={handleSubmit}
+          >
+            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+
+            <form className="space-y-6" onSubmit={sendEmail} ref={form}>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="from_name"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="Lalit Pandey..."
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="from_email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="john@gmail.com"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  required
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
+                  placeholder="Hello, I'd like to talk about..."
+                />
+              </div>
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={cn(
+                  "cosmic-button w-full flex items-center justify-center gap-2"
+                )}
+              >
+                {isSubmitting ? "Sending..." : "Send Message"}
+                <Send size={16} />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
